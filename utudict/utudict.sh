9abb30c6f80f772e65e6be82b76f1e8a03967be8
NNN1590 <nnn1590@nnn1590.org>
2021-02-28 03:06:42 +0900

    Update utudict
    * Typofix in utudict.sh (patched -> patches)
    * Update version to 20200821.1
    * Add .gitignore

--- utudict/utudict.sh
+++ utudict/utudict.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
+# Generate dictionary11.txt from UT Dictionary (http://linuxplayers.g1.xrea.com/mozc-ut.html)
+# utudict = UT unified dictionary
+
 set -e +f
-declare _BASE_DIR="$(cd "$(dirname "${0}")"; pwd)"
+declare _BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${0}}")"; pwd)"
 cd "${_BASE_DIR}"
 
 function main() {
-	declare -r _MOZC_UTU_VERSION="20200821.1"
+	declare -r _MOZC_UTU_VERSION="20210123.1"
 	declare -r _MOZC_UTU_DIR_NAME="mozcdic-ut-${_MOZC_UTU_VERSION}"
 	declare -r _MOZC_UTU_ARCHIVE_FILE_NAME="${_MOZC_UTU_DIR_NAME}.tar.bz2"
-	declare -r _MOZC_UTU_URL="https://osdn.dl.osdn.jp/users/26/26575/${_MOZC_UTU_ARCHIVE_FILE_NAME}"
-	rm -r "${_MOZC_UTU_DIR_NAME}"
+	declare -r _MOZC_UTU_URL="https://osdn.net/users/utuhiro/pf/utuhiro/dl/${_MOZC_UTU_ARCHIVE_FILE_NAME}"
+	declare -r _MOZC_UTU_PATCH_FILE="${_BASE_DIR}/patches/patch-${_MOZC_UTU_VERSION}.diff"
+
+	rm -rf "${_MOZC_UTU_DIR_NAME}"
 	wget -nc "${_MOZC_UTU_URL}"
 	tar xf "${_MOZC_UTU_ARCHIVE_FILE_NAME}"
 	cd "${_MOZC_UTU_DIR_NAME}"
-	patch -Np1 < "${_BASE_DIR}/patched/patch-1.diff"
+	[ -f "${_MOZC_UTU_PATCH_FILE}" ] && patch -Np1 < "${_MOZC_UTU_PATCH_FILE}"
 	ln -s ../../.. mozc/mozc
 	cd src
 	chmod +x make-dictionaries.sh
